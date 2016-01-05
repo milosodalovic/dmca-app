be6e7378339056ecd5e9ee5126327b0d3ae8d139
milosodalovic <oddomir@hotmail.com>
2016-01-05 20:33:47 +0100

    zavrsen video 4 - database setup and authentication.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,7 +23,7 @@
 */
 
 Route::group(['middleware' => ['web']], function () {
-//    Route::auth();
+    Route::auth();
     Route::get('/', 'PagesController@home');
     Route::controllers([
         'auth'      => 'Auth\AuthController',

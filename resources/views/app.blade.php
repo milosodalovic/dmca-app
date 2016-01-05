b2ca1716d3410dd80d98faa8e64d8455d35176b3
milosodalovic <oddomir@hotmail.com>
2016-01-06 00:47:59 +0100

    zavrsen video 6 - a touch of styling. dodato malo css-a kroz elixir, gulp, scss i sl

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>DMCA App</title>
 
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/css/all.css" rel="stylesheet">
 
 </head>
 <body>
